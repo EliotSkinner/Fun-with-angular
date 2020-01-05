f2cba6d1758971d6e86176948702fff9bf527421
Eliot Skinner <Someone@DontEvenTry.org>
2020-01-05 11:50:15 -0600

    Created two services to handle asynchronous data and for loosely-coupled communication between classes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,16 +10,22 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent implements OnInit {
   hero: Hero = {id: 1, name: 'Rickt', url: '../assets/spear02.PNG'};
 
-  heroes = HEROES;
+  heroes: Hero[];
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   selectedHero: Hero;
   onSelect(hero: Hero) {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 
 }
